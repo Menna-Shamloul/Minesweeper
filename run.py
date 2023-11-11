54652b0154643344ad97c57758cc7c683cb13a04
Menna Shamloul <menna.shamloul92@gmail.com>
2023-11-11 22:21:26 +0000

    plant random bombs in board

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import random
 # create a board to represent the minesweeper game
 class Board:
     def _init_(self, dim_size, num_bombs):
@@ -14,6 +15,21 @@ class Board:
     def make_new_board(self):
         # generate a new board
         board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+
+        # plant the bombs
+        bombs_planted = 0
+        while bombs_planted < self.num_bombs:
+            loc = random.randint(0, self.dim_size**2 - 1)
+            row = loc // self.dim_size
+            col = loc % self.dim_size
+
+            if board[row][col] == '*':
+                continue
+
+            board[row][col] = '*' # plant the bomb
+            bombs_planted += 1
+        
+        return board
 
 
 # play the game
