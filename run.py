5c9ae05e769d3ebe00a3bfa803aafd27f16bf97c
Menna Shamloul <menna.shamloul92@gmail.com>
2023-11-11 21:27:57 +0000

    create play function

--- run.py
+++ run.py
@@ -1,3 +1,2 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# play the game
+def play(dim_size=10, num_bombs=10):
