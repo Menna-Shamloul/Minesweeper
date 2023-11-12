e41bb46dddbdaf9b5f10805adf5cd7635b689ae3
Menna Shamloul <menna.shamloul92@gmail.com>
2023-11-12 01:09:28 +0000

    add ways to end the game loop

--- run.py
+++ run.py
@@ -126,4 +126,16 @@ def play(dim_size=10, num_bombs=10):
     while len(board.dug) < board.dim_size ** 2 - num_bombs:
         print(board)
         user_input = re.split(',(\\s)*', input("Where would you like to dig? Input as row,col: ")) 
-        
+        row, col = int(user_input[0]), int(user_input[-1])
+        if row < 0 or row >=board.dim_size or col < 0 or col >=dim_size:
+            print("You provided wrong row or column. Try again.")
+            continue
+        safe = board.dig(row, col)
+        if not safe:
+            break # game over
+    if safe:
+        print(" CONGRATULATIONS! You just beat Minesweeper")
+    else:
+        print("SORRY GAME OVER")
+        board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
+        print(board)
