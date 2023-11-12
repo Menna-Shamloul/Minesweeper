9b0536577624f80b3c1493a44552ec2f760bfc2e
Menna Shamloul <menna.shamloul92@gmail.com>
2023-11-12 00:46:22 +0000

    add import re

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+import re
 # create a board to represent the minesweeper game
 class Board:
     def __init__(self, dim_size, num_bombs):
@@ -120,3 +121,9 @@ class Board:
 def play(dim_size=10, num_bombs=10):
     # create the board and plant the bombs
     board = Board(dim_size, num_bombs)
+    safe = True
+
+    while len(board.dug) < board.dim_size ** 2 - num_bombs:
+        print(board)
+        user_input = re.split(',(\\s)*', input("Where would you like to dig? Input as row,col: ")) 
+        
